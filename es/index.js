540a5638ce6fb71ff76550e746db2a046d3ea426
liziqiang <liziqiang@corp.netease.com>
2024-04-10 10:56:33 +0800

    feat: 补充ts类型

--- es/index.js
+++ es/index.js
@@ -6,45 +6,49 @@ var mapEnv = {
 };
 var TIMEOUT = 30000;
 var YSF = {};
-
 YSF.init = function (appKey, objParams, env) {
-  // 日志上报
-  var img = new Image(); // ua信息在request headers中有带过去，不需要在链接重新传了～
+  try {
+    // 日志上报
+    var img = new Image();
+    // ua信息在request headers中有带过去，不需要在链接重新传了～
+    img.src = "https://qiyukf.com/sdk/res/img/loading.gif?t=" + new Date().getTime() + "&k=" + appKey;
+  } catch (error) {
+    console.log(error);
+  }
 
-  img.src = "https://qiyukf.com/sdk/res/img/loading.gif?t=" + new Date().getTime() + "&k=" + appKey;
+  // eslint-disable-next-line compat/compat
   var promise = new Promise(function (resolve, reject) {
     if (!appKey) {
       reject(new Error('appKey必填'));
     }
-
     if (Object.prototype.toString.call(objParams) !== '[object Object]') {
       reject(new Error('参数类型错误,第二个参数应该是object'));
     }
-
     var ret = [];
-
     if (objParams) {
       for (var item in objParams) {
         console.log(item);
         ret.push(item + "=" + objParams[item]);
       }
     }
-
     var params = ret.length ? "?" + ret.join('&') : '';
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.async = true;
-    script.timeout = TIMEOUT;
-    script.src = "https://" + mapEnv[env || 'online'] + "/script/" + appKey + ".js" + params;
+    if (env && !mapEnv[env]) {
+      script.src = "//" + env + "/script/" + appKey + ".js" + params;
+    } else {
+      script.src = "//" + mapEnv[env || 'online'] + "/script/" + appKey + ".js" + params;
+    }
+    // eslint-disable-next-line prefer-const
     var timeout;
-
     function onScriptComplete(errType, event) {
       script.onerror = null;
       script.onload = null;
       var errorMsg = '';
-      clearTimeout(timeout); // 加载成功
-
+      clearTimeout(timeout);
+      // 加载成功
       if (window.ysf) {
         window.ysf('onready', function () {
           // sdk执行完成
@@ -59,31 +63,28 @@ YSF.init = function (appKey, objParams, env) {
         } else {
           // timeout error
           errorMsg = 'SDK_LOADING_TIMEOUT';
-        } // 上报错误信息
-
-
-        var _img = new Image();
-
-        _img.src = "https://qiyukf.com/sdk/res/img/loading.gif?t=" + new Date().getTime() + "&k=" + appKey + "&error=" + errorMsg;
+        }
+        // 上报错误信息
+        try {
+          var _img = new Image();
+          _img.src = "https://qiyukf.com/sdk/res/img/loading.gif?t=" + new Date().getTime() + "&k=" + appKey + "&error=" + errorMsg;
+        } catch (error) {
+          console.log(error);
+        }
         reject(new Error(errorMsg));
       }
     }
-
     timeout = setTimeout(function () {
       return onScriptComplete('timeout');
     }, TIMEOUT);
-
     script.onerror = function (event) {
       return onScriptComplete('loaderror', event);
     };
-
     script.onload = function (event) {
       return onScriptComplete('onload', event);
     };
-
     head.appendChild(script);
   });
   return promise;
 };
-
 export default YSF;
