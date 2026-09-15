349d8b3733cf9b16c08674036942b551da8a3b99
stickycandy <liziqiangrui@hotmail.com>
2026-09-15 15:06:52 +0800

    feat: 异常资源移除

--- dist/index.js
+++ dist/index.js
@@ -13,14 +13,6 @@ var mapEnv = {
 var TIMEOUT = 30000;
 var YSF = {};
 YSF.init = function (appKey, objParams, env) {
-  try {
-    // 日志上报
-    var img = new Image();
-    // ua信息在request headers中有带过去，不需要在链接重新传了～
-    img.src = "https://ysf.qiyukf.net/141d0a1e2cfbc6f64401afd132094978.gif?t=" + new Date().getTime() + "&k=" + appKey;
-  } catch (error) {
-    console.log(error);
-  }
 
   // eslint-disable-next-line compat/compat
   var promise = new Promise(function (resolve, reject) {
@@ -68,13 +60,6 @@ YSF.init = function (appKey, objParams, env) {
         } else {
           // timeout error
           errorMsg = 'SDK_LOADING_TIMEOUT';
-        }
-        // 上报错误信息
-        try {
-          var _img = new Image();
-          _img.src = "https://ysf.qiyukf.net/141d0a1e2cfbc6f64401afd132094978.gif?t=" + new Date().getTime() + "&k=" + appKey + "&error=" + errorMsg;
-        } catch (error) {
-          console.log(error);
         }
         reject(new Error(errorMsg));
       }
